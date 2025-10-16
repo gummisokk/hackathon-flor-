38a52d4ae5d1c1b7949699760ecbbe5b47459906
Holly <holibuksa@gmail.com>
2025-10-16 11:05:46 +0200

    leseSkip

--- SmartPort/src/smartPort/App.java
+++ SmartPort/src/smartPort/App.java
@@ -1,26 +1,38 @@
 package smartPort;
 
+import java.io.File;
 import java.util.Scanner;
-
-import javax.sound.sampled.Port;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        Scanner scanner = new Scanner(System.in);
+        File file = new File("innDataSet/altiein.txt");
+
+        //Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(file);
 
         int P = scanner.nextInt();
+        System.out.println(P);
 
         Port[] ports = new Port[P];
-
         for (int i = 0; i < P; i++) {
             //les inn info om portane
             String[] line = scanner.nextLine().split(",");
             ports[i] = new Port(line[0]);
             for (int j = 0; j < Integer.parseInt(line[1]); j++) {
                 ports[i].addItem(line[2 + j]);
+            }
+        }
+
+        int S = scanner.nextInt();
+        Ship[] ships = new Ship[S];
+        for (int i = 0; i < S; i++) {
+            //les inn info om skipa
+            String[] line = scanner.nextLine().split(",");
+            ships[i] = new Ship(line[0], line[1], line[2]);
+            for (int j = 0; j < Integer.parseInt(line[3]); j++) {
+                ports[i].addRequest(line[4 + j]);
             }
         }
     }
